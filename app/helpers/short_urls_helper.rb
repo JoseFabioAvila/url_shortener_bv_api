4e82771b6a36a9952d3cc578d973979e1deaf962
josefabioavila <fabio.avila@dotcreek.com>
2019-11-11 00:08:52 -0600

    Add base62 encode and decode algorithm on short url helper

--- app/helpers/short_urls_helper.rb
+++ app/helpers/short_urls_helper.rb
@@ -1,2 +1,27 @@
 module ShortUrlsHelper
+  KEYS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.freeze
+  BASE = 62.freeze
+
+  def self.encode_base_62(id)
+    code = ''
+
+    while id > 0
+      code.prepend KEYS[id % BASE]
+      id = id / BASE
+    end
+
+    code
+  end
+
+  def self.decode_base_62(code)
+    id = 0
+    digit = 0
+
+    code.reverse.each_char do |char|
+      id += KEYS.index(char) * (BASE ** digit)
+      digit += 1
+    end
+
+    return id
+  end
 end
